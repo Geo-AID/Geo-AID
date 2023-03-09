9fd1ff29d655e49b97fa09213e02d5865eda3995
DragonGamesStudios <margos.michal@gmail.com>
2023-03-09 11:19:26 +0100

    Expression overhaul preparation

--- src/script/figure.rs
+++ src/script/figure.rs
@@ -1,14 +1,14 @@
 use std::sync::Arc;
 
-use super::{unroll::PointMeta, Expression, Weighed};
+use super::{unroll::PointMeta, ExprKind, Weighed};
 
 /// Defines the visual data of the figure.
 #[derive(Debug)]
 pub struct Figure {
     /// The points to be displayed
-    pub points: Vec<(Arc<Weighed<Expression>>, PointMeta)>,
+    pub points: Vec<(Arc<Weighed<ExprKind>>, PointMeta)>,
     /// The lines to be displayed
-    pub lines: Vec<Arc<Weighed<Expression>>>,
+    pub lines: Vec<Arc<Weighed<ExprKind>>>,
     /// Angles to be displayed
     pub angles: Vec<(Arc<Weighed<Expression>>, u8)>, // This u8 refers to number of arcs in an angle!
     /// The canvas size.
---
+++ tests/optimized-with-rules.geo
@@ -0,0 +1,9 @@
+# Created to test issue #59
+let A, B, C, D = Point();
+
+let E = intersection(AB, CD);
+
+let x, y = AE, DE;
+x, y = CE, BC;
+
+x = y;
