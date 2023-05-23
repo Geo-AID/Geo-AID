e0ca5a55da5b15a4d22c909f7d829ed54a357861
DragonGamesStudios <margos.michal@gmail.com>
2023-05-23 09:36:21 +0200

    Circleclips start

--- src/generator/magic_box.rs
+++ src/generator/magic_box.rs
@@ -1,6 +1,6 @@
 use std::f64::consts::PI;
 
-use super::{Adjustable, Complex};
+use super::{Adjustables, Complex, WithQuality};
 
 /// Performs an adjustment in a random direction.
 ///
@@ -9,37 +9,55 @@ use super::{Adjustable, Complex};
 /// * `quality` - a vector of qualities assigned to each point. Epsilon in the formula.
 /// * `adjustment_magnitude` - the magnitude to apply to the adjustment (how much of a jump to allow). Eta in the formula.
 pub fn adjust(
-    adjustables: Vec<(Adjustable, f64)>,
+    adjustables: Adjustables,
     adjustment_magnitude: f64,
-) -> Vec<(Adjustable, f64)> {
-    adjustables
-        .into_iter()
-        .map(|(adj, quality)| {
-            match adj {
-                Adjustable::Point(point) => {
-                    let direction = 2.0 * rand::random::<f64>() * PI;
+) -> Adjustables {
+    Adjustables {
+        points: adjustables.points
+            .into_iter()
+            .map(|WithQuality {quality, value}| {
+                let direction = 2.0 * rand::random::<f64>() * PI;
 
                     let unit = Complex::new(direction.cos(), direction.sin());
                     let offset = unit * adjustment_magnitude * (1.0 - quality);
 
-                    (Adjustable::Point(point + offset), quality)
-                }
-                Adjustable::Real(val) => {
-                    let direction = if rand::random::<u8>() & 1 == 0 {
-                        1.0
-                    } else {
-                        -1.0
-                    };
-
-                    (
-                        // Adjust by a relative value based on quality and randomly chosen direction (+/-)
-                        Adjustable::Real(
-                            val + direction * adjustment_magnitude * val * (1.0 - quality),
-                        ),
+                    WithQuality {
                         quality,
-                    )
+                        value: value + offset
+                    }
+            })
+            .collect(),
+        scalars: adjustables.scalars
+            .into_iter()
+            .map(|WithQuality {quality, value}| {
+                let direction = if rand::random::<u8>() & 1 == 0 {
+                    1.0
+                } else {
+                    -1.0
+                };
+
+                WithQuality {
+                    quality,
+                    // Adjust by a relative value based on quality and randomly chosen direction (+/-)
+                    value: value + direction * adjustment_magnitude * value * (1.0 - quality)
                 }
-            }
-        })
-        .collect()
+            })
+            .collect(),
+        circle_clips: adjustables.circle_clips
+            .into_iter()
+            .map(|WithQuality {quality, value}| {
+                let direction = if rand::random::<u8>() & 1 == 0 {
+                    1.0
+                } else {
+                    -1.0
+                };
+
+                WithQuality {
+                    quality,
+                    // Adjust by a relative value based on quality and randomly chosen direction (+/-)
+                    value: value + direction * adjustment_magnitude * value * (1.0 - quality)
+                }
+            })
+            .collect()
+    }
 }
