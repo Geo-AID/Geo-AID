96f2019eef7c6e4772fb6fd5e2d2af7af1f68fd0
DragonGamesStudios <margos.michal@gmail.com>
2024-09-15 20:38:51 +0200

    geo-aid-internal/format/json.rs docs

--- crates/geo-aid-internal/src/format/json.rs
+++ crates/geo-aid-internal/src/format/json.rs
@@ -1,11 +1,17 @@
+//! Geo-AID is capable of outputting figures in JSON, according to the schema at top level
+//! in the repo. This is the format writer responsible for this.
+
 use std::{fs::File, io::Write, path::Path};
 
 use geo_aid_figure::Figure;
 
+/// The JSON format writer.
 #[derive(Default)]
 pub struct Json;
 
 impl Json {
+    /// Output the figure as JSON.
+    ///
     /// # Errors
     /// Returns an error when there was a problem with creating the output file.
     /// # Panics
